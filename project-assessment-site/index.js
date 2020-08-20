cf93ae3860f2e7aaad6ee345735c589c4308c81d
HatimAbdullah <HatimAlqiamh@gmail.com>
2020-08-20 17:10:43 +0300

    retun to normal

--- project-assessment-site/index.js
+++ project-assessment-site/index.js
@@ -4,29 +4,8 @@ var http = require('http');
 
 app.get('/', (req, res) => {
  
-var options = {
-  host: '127.0.0.1:31511',  // process.env.OFFICE_SERVICE_HOST,
-  path: '/office'
- };
+app.use(express.static('public'))
 
-callback = function(response) {
-  var str = '';
-
-  //another chunk of data has been received, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
-
-  //the whole response has been received, so we just print it out here
-  response.on('end', function () {
-    return str;
-  });
-}
-
-res = http.request(options, callback).end();
- res.send('we got: %s', res )
-})
-
-app.listen(8021, () => {
-    console.log("Project Assessment Website: running on port 80");
+app.listen(8000, () => {
+    console.log("Project Assessment Website: running on port 8000");
 });

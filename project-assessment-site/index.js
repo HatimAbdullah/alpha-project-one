325a992582806f10e2dd27f4778c4d7424e6fab1
HatimAbdullah <HatimAlqiamh@gmail.com>
2020-08-21 19:27:51 +0300

    working ingress

--- project-assessment-site/index.js
+++ project-assessment-site/index.js
@@ -1,11 +1,8 @@
 var express = require("express");
 var app = express();
-var http = require('http');
-
-app.get('/', (req, res) => {
  
 app.use(express.static('public'))
 
-app.listen(8000, () => {
-    console.log("Project Assessment Website: running on port 8000");
+app.listen(80, () => {
+    console.log("Project Assessment Website: running on port 80");
 });
